668f17e9336bfaa0253e6c1df91ccf1930dc638a
Corey Barr <corey.jbarr@gmail.com>
2020-08-12 14:01:38 -0500

    server bug

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const pool = require('./modules/pool.js');
+const pg = require('pg');
+const pool = require('./modules/pool');
+
 
 const postRouter = require('./routes/posts.router.js');
 
@@ -9,18 +11,17 @@ const postRouter = require('./routes/posts.router.js');
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
 
 app.use('/posts', postRouter);
 
-/** ----------SERVER ---------- **/
-pool.connect((function (err) {
+/** ---------- START SERVER ---------- **/
+pool.connect(function (err) {
 
     if (!err) {
         console.log("Database is connected ... ");
     } else {
         console.log("Error connecting database ... ");
     }
-}));
+});
